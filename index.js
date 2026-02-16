44ee152c796609346236f2826482a6553a340ae1
Teste1010 <contatonovely@gmail.com>
2026-02-15 21:17:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("BOT ONLINE 🚀");
+    res.send("BOT ONLINE 🚀");
+});
+
+app.use(express.json());
+
+app.post("/webhook", (req, res) => {
+    console.log("Webhook recebido:");
+    console.log(req.body);
+
+    res.sendStatus(200);
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log("Servidor rodando");
+    console.log("Servidor rodando");
 });
