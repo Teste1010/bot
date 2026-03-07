589e04e26ffb9462a22939137058a8d1b6e3cab3
Teste1010 <contatonovely@gmail.com>
2026-03-07 17:23:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const fetch = require("node-fetch");
+
 const app = express();
 
-let pagamentoPendente = false;
+let coinsPendentes = 0;
 
 app.use(express.json());
 
@@ -9,28 +11,58 @@ app.get("/", (req, res) => {
   res.send("BOT ONLINE");
 });
 
-// RECEBE WEBHOOK DO MERCADO PAGO
-app.post("/webhook", (req, res) => {
+app.post("/webhook", async (req, res) => {
   console.log("Webhook recebido:");
   console.log(req.body);
 
-  pagamentoPendente = true;
+  try {
+
+    const paymentId = req.body.data.id;
+
+    const resposta = await fetch(
+      https://api.mercadopago.com/v1/payments/${paymentId},
+      {
+        headers: {
+          Authorization: Bearer SEU_ACCESS_TOKEN_AQUI
+        }
+      }
+    );
+
+    const pagamento = await resposta.json();
+
+    const valor = pagamento.transaction_amount;
+
+    console.log("Valor pago:", valor);
+
+    coinsPendentes += Math.floor(valor);
+
+  } catch (erro) {
+    console.log("Erro webhook:", erro);
+  }
 
   res.sendStatus(200);
 });
 
-// ESP32 CONSULTA SE TEM PAGAMENTO
 app.get("/check", (req, res) => {
-  if (pagamentoPendente) {
-    pagamentoPendente = false;
-    res.send("PAGAR");
+
+  if (coinsPendentes > 0) {
+
+    let coins = coinsPendentes;
+
+    coinsPendentes = 0;
+
+    res.send(String(coins));
+
   } else {
-    res.send("AGUARDANDO");
+
+    res.send("0");
+
   }
+
 });
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log("Servidor rodando");
 });
